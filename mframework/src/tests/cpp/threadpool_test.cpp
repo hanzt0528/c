3687e90608d5327849f2e4d6578b345cfb485c3d
hanzt <32769491@qq.com>
2023-11-21 13:57:45 +0800

    add threadpool

---
+++ mframework/src/tests/cpp/threadpool_test.cpp
@@ -0,0 +1,25 @@
+//#include "threadpool.h"
+#include "threadpool2.h"
+#include <iostream>
+
+
+void test()
+{
+    std::cout <<"test!"<<std::endl;
+};
+
+//g++ threadpool_test.cpp 
+int main(int argc ,char* argv[])
+{
+    std::cout <<"hello!"<<std::endl;
+
+    ThreadPool pool(5);
+
+    auto result = pool.enqueue([](int answer){return answer;},42);
+
+    std::cout <<result.get()<<std::endl;
+
+    auto result2 = pool.enqueue(test); 
+    
+    return 0;
+}
