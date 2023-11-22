e772a3ca807da9eb9b6aa11336cb7fc54c6a4bfa
hanzt <32769491@qq.com>
2023-11-22 10:26:17 +0800

    threadpool3

--- mframework/src/tests/cpp/threadpool_test.cpp
+++ mframework/src/tests/cpp/threadpool_test.cpp
@@ -1,25 +1,27 @@
-//#include "threadpool.h"
-#include "threadpool2.h"
+// #include "threadpool.h"
+// #include "threadpool2.h"
+#include "threadpool3.h"
 #include <iostream>
-
 
 void test()
 {
-    std::cout <<"test!"<<std::endl;
+    std::cout << "test!" << std::endl;
 };
 
-//g++ threadpool_test.cpp 
-int main(int argc ,char* argv[])
+// g++ threadpool_test.cpp
+int main(int argc, char *argv[])
 {
-    std::cout <<"hello!"<<std::endl;
+    std::cout << "hello!" << std::endl;
 
     ThreadPool pool(5);
 
-    auto result = pool.enqueue([](int answer){return answer;},42);
+    auto result = pool.enqueue([](int answer)
+                               { return answer; },
+                               42);
 
-    std::cout <<result.get()<<std::endl;
+    std::cout << result.get() << std::endl;
 
-    auto result2 = pool.enqueue(test); 
-    
+    auto result2 = pool.enqueue(test);
+
     return 0;
 }
