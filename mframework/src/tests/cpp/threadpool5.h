98b2d730e6f20135c99d3a3d0da794fb8896e72f
hanzt <32769491@qq.com>
2023-11-27 09:14:18 +0800

    11

--- mframework/src/tests/cpp/threadpool5.h
+++ mframework/src/tests/cpp/threadpool5.h
@@ -1,82 +1,86 @@
 #include <vector>
 #include <queue>
 #include <thread>
-#include <mutex>
 #include <future>
-#include <condition_variable>
+#include <mutex>
 #include <functional>
 #include <stdexcept>
+#include <condition_variable>
+
 
 class ThreadPool
 {
-private:
+    private:
     std::vector<std::thread> workers;
     std::queue<std::function<void()>> tasks;
     std::mutex queue_mutex;
     std::condition_variable condition;
     bool stop;
 
-public:
+    public:
     ThreadPool(size_t threads)
-        : stop(false)
     {
-        for (size_t i = 0; i < threads; i++)
+        for(size_t i = 0; i < threads; i++)
         {
             workers.emplace_back(
-                std::thread([this]
-                            {
+                std::thread([this]{
+
                     for(;;)
                     {
                         std::function<void()> task;
                         {
-                            std::unique_lock<std::mutex> lock(this->queue_mutex);
-                            condition.wait(lock,[this]{return this->stop || !this->tasks.empty();});
-
+                            std::unique_lock<std::mutex> lock(queue_mutex);
+                            condition.wait(lock,[this]{return this->stop || ! this->tasks.empty();});
                             if(this->stop && this->tasks.empty())
                                 return;
 
-                            task = std::move(this->tasks.front());
-                            this->tasks.pop();
+                            task = std::move(tasks.front());
+                            tasks.pop();
                         }
-
                         task();
-                    } }));
+                    }
+                })
+            );
         }
-    };
+    }
 
-    template <class F, class... Args>
-    auto enqueue(F &&f, Args... args) -> std::future<typename std::result_of<F(Args...)>::type>
+
+     //typename
+     //return_type()
+     //return res;
+    template<class F,class... Args>
+    auto enqueue(F &&f,Args... args) ->std::future<typename std::result_of<F(Args...)>::type>
     {
         using return_type = typename std::result_of<F(Args...)>::type;
 
         auto task = std::make_shared<std::packaged_task<return_type()>>(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+            std::bind(std::forward<F>(f),std::forward<Args>(args)...)
+        );
 
         std::future<return_type> res = task->get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
+            if(this->stop)
+                throw std::runtime_error("enqueue on stopped threadpool");
 
-            if (stop)
-                throw std::runtime_error("enqueue on stopped ThreadPool!");
-
-            tasks.emplace([task]
-                          { (*task)(); });
+            tasks.emplace([task]{(*task)();});
         }
 
         condition.notify_one();
+
         return res;
     }
 
     ~ThreadPool()
     {
-        std::cout << "~ThreadPool"<<std::endl;
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
             stop = true;
+            condition.notify_all();
         }
-        condition.notify_all();
-
-        for (std::thread &worker : workers)
+        for(std::thread &worker:workers)
+        {
             worker.join();
+        }
     }
 };
--- mframework/src/tests/cpp/threadpool_test.cpp
+++ mframework/src/tests/cpp/threadpool_test.cpp
@@ -1,6 +1,8 @@
 // #include "threadpool.h"
 // #include "threadpool2.h"
-#include "threadpool3.h"
+// #include "threadpool3.h"
+//#include "threadpool4.h"
+#include "threadpool5.h"
 #include <iostream>
 
 void test()
@@ -16,12 +18,24 @@ int main(int argc, char *argv[])
     ThreadPool pool(5);
 
     auto result = pool.enqueue([](int answer)
-                               { return answer; },
+                               {
+                                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+                                return answer; 
+                                },
                                42);
 
-    std::cout << result.get() << std::endl;
+    auto result2 = pool.enqueue([](int answer)
+                               {
+                                 std::this_thread::sleep_for(std::chrono::milliseconds(2000)); 
+                                return answer; },
+                               43);
 
-    auto result2 = pool.enqueue(test);
+
+    auto result3 = pool.enqueue(test);
+
+    std::cout << result.get() << std::endl;
+    std::cout << result2.get() << std::endl;
 
     return 0;
 }
